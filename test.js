7ad25af9ac96eab6de7bbcfe8be4aef18e97e50d
Olexii Kolomiiets <alexkolomiits@gmail.com>
2018-03-08 22:31:23 +0200

    refactored code

--- test.js
+++ test.js
@@ -4,36 +4,59 @@ describe("extractCurrencyValue", function() {
     assert.strictEqual(search([1], 1), '0-0');
   });
   
-  it("[1], 0 => NaN", function() {
-    assert.strictEqual(search([1], 0), 'NaN');
+  it("[2,1], 1 => 1-1", function() {
+    assert.strictEqual(search([2,1], 1), '1-1');
   });
-
-  it("[1,2,3], 1 => 0-0,2-2", function() {
-    assert.strictEqual(search([1,2,1], 1), '0-0,2-2');
+  
+  it("[3,2,1], 1 => 2-2", function() {
+    assert.strictEqual(search([3,2,1], 1), '2-2');
   });
-
-  it("[1,2,3], 2 => 1-1", function() {
-    assert.strictEqual(search([1,2,3], 2), '1-1');
+  
+  it("[1], 0 => notFind", function() {
+    assert.strictEqual(search([1], 0), 'notFind');
   });
-
-  it("[2,2,3], 3 => 0-0,2-2", function() {
-    assert.strictEqual(search([2,2,3], 3), '2-2');
+  
+  it("[0], 1 => notFind", function() {
+    assert.strictEqual(search([0], 1), 'notFind');
   });
-
-  it("[2,2,5,3], 12 => 0-3", function() {
-    assert.strictEqual(search([2,2,5,3], 12), '0-3');
+  
+  it("[4,2,4,14,5], 6 => 0-1", function() {
+    assert.strictEqual(search([4,2,4,14,5], 6), '0-1');
   });
 
   it("[2,2,5,3], 7 => 1-2", function() {
     assert.strictEqual(search([2,2,5,3], 7), '1-2');
   });
 
-  it("[2,2,2,2], 2 => 0-0,1-1,2-2,3-3", function() {
-    assert.strictEqual(search([2,2,2,2], 2), '0-0,1-1,2-2,3-3');
+  it("[2,2,5,3], 12 => 0-3", function() {
+    assert.strictEqual(search([2,2,5,3], 12), '0-3');
   });
 
-  it("[2,23,2,21], 22 => NaN", function() {
-    assert.strictEqual(search([2,23,2,21], 22), 'NaN');
+  it("[2,2,3], 2 => 0-0", function() {
+    assert.strictEqual(search([2,2,3], 2), '0-0');
   });
+
+  it("[1,2,3], 2 => 1-1", function() {
+    assert.strictEqual(search([1,2,3], 2), '1-1');
+  });
+
+  it("[2,3,5,10], 15 => 2-3", function() {
+    assert.strictEqual(search([2,3,5,10], 15), '2-3');
+  });
+
+  it("[1,1,1,10], 12 => 1-3", function() {
+    assert.strictEqual(search([1,1,1,10], 12), '1-3');
+  });
+
+  it("[10,1,1,10], 2 => 1-2", function() {
+    assert.strictEqual(search([10,1,1,10], 2), '1-2');
+  });
+
+  it("[10,10,1000,100,15], 1125 => 1-4", function() {
+    assert.strictEqual(search([10,10,1000,100,15], 1125), '1-4');
+  });
+
+
+
 
 });
